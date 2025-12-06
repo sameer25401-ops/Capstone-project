8ee8dbb05c620418d6219cf290d0190def229d24
Sameer Sharma <sameer.25401@nst.rishihood.edu.in>
2025-12-06 12:19:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const loginBtn = document.querySelector(".btn");
+
+loginBtn.onclick = function () {
+    alert("Do You Want To Login ?");
+};
+
+
+
+const getStartedBtn = document.querySelector(".cta-btn");
+
+getStartedBtn.onclick = function () {
+    alert("Thank you for choosing BitMorph!");
+};
+
+
+
+
+
+
+
+
+
